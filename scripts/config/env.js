19da6a50a8fd0015ef60a23a5f7f7dab5ac5ee27
Paul Brunner <trite@users.noreply.github.com>
2023-06-21 09:03:44 -0700

    Trying out some different things to get graphile migrate going

--- scripts/config/env.js
+++ scripts/config/env.js
@@ -1,3 +1,36 @@
 /* Use via `node -r @app/config/env path/to/file.js` */
+
+const envContent = require("fs").readFileSync(`${__dirname}/../../.env`, "utf8");
+
+// console.log(`--- ${envContent} ---`)
+
 require("dotenv").config({ path: `${__dirname}/../../.env` });
-require("./extra");
+
+console.log('\n============\n');
+console.log(`Loading environment variables from ${__dirname}/../../.env`);
+console.log(`DATABASE_URL: ${process.env.DATABASE_URL}`);
+console.log(`SHADOW_DATABASE_URL: ${process.env.SHADOW_DATABASE_URL}`);
+console.log('\n============\n');
+
+// These are the connection strings for the DB and the test DB.
+// NOTE: in production you probably want to add ?ssl=1 to force SSL usage.
+// NOTE: 'psql' does not like ?ssl=1, use ?sslmode=require for psql.
+// NOTE: these used to be in `.env` but now it is used by docker-compose we can't use expansions
+process.env.DATABASE_URL =
+  process.env.DATABASE_URL ||
+  `postgres://${process.env.DATABASE_OWNER}:${process.env.DATABASE_OWNER_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}`;
+process.env.AUTH_DATABASE_URL =
+  process.env.AUTH_DATABASE_URL ||
+  `postgres://${process.env.DATABASE_AUTHENTICATOR}:${process.env.DATABASE_AUTHENTICATOR_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}`;
+process.env.SHADOW_DATABASE_URL =
+  process.env.SHADOW_DATABASE_URL ||
+  `postgres://${process.env.DATABASE_OWNER}:${process.env.DATABASE_OWNER_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}_shadow`;
+process.env.SHADOW_AUTH_DATABASE_URL =
+  process.env.SHADOW_AUTH_DATABASE_URL ||
+  `postgres://${process.env.DATABASE_AUTHENTICATOR}:${process.env.DATABASE_AUTHENTICATOR_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}_shadow`;
+
+// Always overwrite test database URL
+process.env.TEST_DATABASE_URL = `postgres://${process.env.DATABASE_OWNER}:${process.env.DATABASE_OWNER_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}_test`;
+
+// https://docs.cypress.io/guides/guides/environment-variables.html#Option-3-CYPRESS
+process.env.CYPRESS_ROOT_URL = process.env.ROOT_URL;
