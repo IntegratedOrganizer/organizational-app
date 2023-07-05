67a21927033cdb325a0143322b554fecee4df89c
Paul Brunner <trite@users.noreply.github.com>
2023-07-04 20:38:12 -0700

    Fix data insert

--- db/init/01-data.sql
+++ db/init/01-data.sql
@@ -1,16 +1,6 @@
 \connect organizational-app;
 
-INSERT INTO public.users(first_name)
-VALUES ('Sammy'), ('Paul');
-
--- /*Create some dummy users*/
--- INSERT INTO public.user (username) VALUES
--- ('Benjie'),
--- ('Singingwolfboy'),
--- ('Lexius');
-
--- /*Create some dummy posts*/
--- INSERT INTO public.post (title, body, author_id) VALUES
--- ('First post example', 'Lorem ipsum dolor sit amet', 1),
--- ('Second post example', 'Consectetur adipiscing elit', 2),
--- ('Third post example', 'Aenean blandit felis sodales', 3);
+INSERT INTO public.users(user_name, first_name, last_name)
+VALUES
+  ('samster42', 'Sammy', 'Samster'),
+  ('trite', 'Paul', 'Smith');
