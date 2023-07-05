58a0ec087b70a7eaa7c08a3cb8b83317c5774917
Paul Brunner <trite@users.noreply.github.com>
2023-07-04 20:33:05 -0700

    forgot this

--- db/init/00-database.sql
+++ db/init/00-database.sql
@@ -1,26 +1,14 @@
 \connect organizational-app;
 
--- /*Create user table in public schema*/
--- CREATE TABLE public.user (
---     id SERIAL PRIMARY KEY,
---     username TEXT,
---     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- );
+SET client_encoding = 'LATIN1';
 
--- COMMENT ON TABLE public.user IS
--- 'Forum users.';
-
--- /*Create post table in public schema*/
--- CREATE TABLE public.post (
---     id SERIAL PRIMARY KEY,
---     title TEXT,
---     body TEXT,
---     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
---     author_id INTEGER NOT NULL REFERENCES public.user(id)
--- );
-
--- COMMENT ON TABLE public.post IS
--- 'Forum posts written by a user.';
+CREATE OR REPLACE FUNCTION trigger_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+   NEW.updated_at = NOW();
+   RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
 CREATE TABLE IF NOT EXISTS public.users (
 	id serial PRIMARY KEY,
